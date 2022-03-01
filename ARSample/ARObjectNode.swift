ccb686fefc9df4463e7519cf25d3ed98920666c6
ji-no <jino.jpn@gmail.com>
2022-03-01 22:54:26 +0900

    remove floating action

--- ARSample/ARObjectNode.swift
+++ ARSample/ARObjectNode.swift
@@ -28,10 +28,8 @@ class ARObjectNode: SCNNode {
     enum State {
         case idle
         case selected
-        case canceling
     }
     private var state: State = .idle
-    private var startPositionY: Float = 0.0
 
     init(type: ObjectType = .AirForce, position: SCNVector3) {
         super.init()
@@ -61,16 +59,13 @@ class ARObjectNode: SCNNode {
     }
     
     private func spawn(position: SCNVector3) {
-        startPositionY = position.y
         self.position = position
-        self.position.y += 0.1
-        state = .selected
         
         let toScale = CGFloat(self.scale.x)
         self.scale = SCNVector3(0, 0, 0)
         let action = SCNAction.scale(to: toScale, duration: 0.2)
         self.runAction(action, forKey: nil) { [weak self] in
-            self?.floatingAction()
+            self?.state = .selected
         }
     }
     
@@ -82,15 +77,12 @@ class ARObjectNode: SCNNode {
         guard state == .idle else { return }
 
         state = .selected
-        riseAction()
     }
     
     func cancel() {
         guard state == .selected else { return }
 
-        state = .canceling
-        removeAllActions()
-        falldownAction()
+        state = .idle
     }
     
     func removeObject() {
@@ -98,42 +90,6 @@ class ARObjectNode: SCNNode {
         let action = SCNAction.scale(to: 0, duration: 0.2)
         self.runAction(action, forKey: nil) { [weak self] in
             self?.removeFromParentNode()
-        }
-    }
-    
-    private func riseAction() {
-        startPositionY = position.y
-
-        let rise = SCNAction.moveBy(x: 0, y: 0.1, z: 0, duration: 0.5)
-        rise.timingMode = .easeInEaseOut
-        self.runAction(rise, forKey: nil) { [weak self] in
-            self?.floatingAction()
-        }
-    }
-    
-    private func floatingAction() {
-        let down = SCNAction.moveBy(x: 0, y: -0.03, z: 0, duration: 1)
-        down.timingMode = .easeInEaseOut
-        let up = SCNAction.moveBy(x: 0, y: 0.03, z: 0, duration: 1)
-        up.timingMode = .easeInEaseOut
-
-        let floating = SCNAction.sequence([down,up])
-        self.runAction(floating, forKey: nil) { [weak self] in
-            self?.floatingAction()
-        }
-    }
-    
-    private func falldownAction() {
-        let fallY = startPositionY - self.position.y
-        let fall = SCNAction.move(by: SCNVector3(0, fallY, 0), duration: 0.3)
-        let up = SCNAction.move(by: SCNVector3(0, 0.02, 0), duration: 0.1)
-        let down = SCNAction.move(by: SCNVector3(0, -0.02, 0), duration: 0.1)
-        fall.timingMode = .easeIn
-        up.timingMode = .easeOut
-        down.timingMode = .easeIn
-        let falldown = SCNAction.sequence([fall,up,down])
-        self.runAction(falldown, forKey: nil) { [weak self] in
-            self?.state = .idle
         }
     }
 
